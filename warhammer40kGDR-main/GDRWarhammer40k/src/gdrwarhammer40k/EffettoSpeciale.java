9987ebbf8c43ea2a66ba3d8c41687b0b1b02cb30
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2026-03-25 09:13:23 +0100

    .

---
+++ warhammer40kGDR-main/GDRWarhammer40k/src/gdrwarhammer40k/EffettoSpeciale.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gdrwarhammer40k;
+
+/**
+ *
+ * @author bernicchi.davide
+ */
+public class EffettoSpeciale {
+    SpaceMarine player;
+    Weapon arma;
+    
+    EffettoSpeciale(SpaceMarine sm, Weapon w){
+        player = sm;
+        arma= w;
+    }
+}
